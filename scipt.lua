04fce935a392563cb6339291ea86b33787b9752c
deiectio0202 <deiectio0202@gmail.com>
2025-08-22 17:11:18 -0300

    Update script.lua

--- scipt.lua
+++ scipt.lua
@@ -1,1 +1,131 @@
+local module = {
+    BanditQuest1 = {
+        {
+            LevelReq = 0,
+            MonsterName = "Bandit [Lv. 5]",
+            Numero = 1,
+            NPC_Position = CFrame.new(1061.4542236328125, 16.459312438964844, 1548.5809326171875),
+            BatPosition = CFrame.new(1195.4478759765625, 16.738540649414062, 1619.4395751953125),
+        }
+    },
+    
+    JungleQuest = {
+        {
+            LevelReq = 10,
+            MonsterName = "Monkeys",
+            Numero = 1,
+            NPC_Position = CFrame.new(-1598.9091796875, 36.88703918457031, 151.84658813476562),
+            BatPosition = CFrame.new(-1612.990478515625, 36.88703918457031, 150.83897399902344),
+        },
+        {
+            LevelReq = 15,
+            MonsterName = "Gorillas",
+            Numero = 2,
+            NPC_Position = CFrame.new(-1598.9091796875, 36.88703918457031, 151.84658813476562),
+            BatPosition = CFrame.new(-1193.3863525390625, 7.77052640914917, -447.0392761230469),
+        },
+        {
+            LevelReq = 20,
+            MonsterName = "Gorilla King",
+            Numero = 3,
+            NPC_Position = CFrame.new(-1598.9091796875, 36.88703918457031, 151.84658813476562),
+            BatPosition = CFrame.new(-1193.3863525390625, 7.77052640914917, -447.0392761230469),
+        }
+    },
+    BuggyQuest1 = {
+        {
+            LevelReq = 30,
+            MonsterName = "pirates",
+            Numero = 1,
+            NPC_Position = CFrame.new(-1140.81103515625, 4.78705358505249, 3828.819091796875),
+            BatPosition = CFrame.new(-1229.1192626953125, 4.787052631378174, 3911.2353515625),
+        },
+        {
+            LevelReq = 40,
+            MonsterName = "Brute",
+            Numero = 2,
+            NPC_Position = CFrame.new(-1140.81103515625, 4.78705358505249, 3828.819091796875),
+            BatPosition = CFrame.new(-1133.075439453125, 14.845051765441895, 4331.6689453125),
+        },
+        {
+            LevelReq = 55,
+            MonsterName = "Chef",
+            Numero = 3,
+            NPC_Position = CFrame.new(-1140.81103515625, 4.78705358505249, 3828.819091796875),
+            BatPosition = CFrame.new(-1113.0457763671875, 13.787052154541016, 4090.375),
+        },  
+    },
+    DesertQuest = {
+        {
+            LevelReq = 60,
+            MonsterName = "Desert Bandit",
+            Numero = 1,
+            NPC_Position = CFrame.new(896.2175903320312, 6.473949909210205, 4390.8984375),
+            BatPosition = CFrame.new(923.3231811523438, 6.483530521392822, 4475.25),
+        },
+        {
+            LevelReq = 75,
+            MonsterName = "Desert Officer",
+            Numero = 2,
+            NPC_Position = CFrame.new(896.2175903320312, 6.473949909210205, 4390.8984375),
+            BatPosition = CFrame.new(923.3231811523438, 6.483530521392822, 4475.25),
+        },
+    },
+    SnowQuest = {
+        {
+            LevelReq = 90,
+            MonsterName = "Snow Bandit",
+            Numero = 1,
+            NPC_Position = CFrame.new(1384.8204345703125, 87.3078842163086, -1295.4405517578125),
+            BatPosition = CFrame.new(1341.9140625, 87.30789184570312, -1368.4342041015625),
+        },
+        {
+            LevelReq = 100,
+            MonsterName = "SnowMan",
+            Numero = 2,
+            NPC_Position = CFrame.new(1384.8204345703125, 87.3078842163086, -1295.4405517578125),
+            BatPosition = CFrame.new(1219.7713623046875, 138.04640197753906, -1491.2850341796875),
+        },
+        {
+            LevelReq = 105,
+            MonsterName = "Yeti",
+            Numero = 3,
+            NPC_Position = CFrame.new(1384.8204345703125, 87.3078842163086, -1295.4405517578125),
+            BatPosition = CFrame.new(1219.7713623046875, 138.04640197753906, -1491.2850341796875),
+        },
+    },
+    MarineQuest2 = {
+        {
+            LevelReq = 120,
+            MonsterName = "Chief Petty Officer",
+            Numero = 1,
+            NPC_Position = CFrame.new(-5044.404296875, 28.687044143676758, 4323.64404296875),
+            BatPosition = CFrame.new(-4936.24169921875, 20.68704605102539, 4022.8466796875),
+        },
+        {
+            LevelReq = 130,
+            MonsterName = "Vice Admiral",
+            Numero = 2,
+            NPC_Position = CFrame.new(-5044.404296875, 28.687044143676758, 4323.64404296875),
+            BatPosition = CFrame.new(-4967.35009765625, 92.86299896240234, 4386.03857421875),
+        },
+    },
+    SkyQuest = {
+        {
+            LevelReq = 150,
+            MonsterName = "Sky Bandit",
+            Numero = 1,
+            NPC_Position = CFrame.new(-4841.04736328125, 717.8450927734375, -2619.81884765625),
+            BatPosition = CFrame.new(-4998.3984375, 278.1017150878906, -2848.9736328125),
+        },
+        {
+            LevelReq = 175,
+            MonsterName = "Dark Master",
+            Numero = 2,
+            NPC_Position = CFrame.new(-4841.04736328125, 717.8450927734375, -2619.81884765625),
+            BatPosition = CFrame.new(-5206.75341796875, 388.68701171875, -2272.59228515625),
+        },
+    },
 
+
+}
